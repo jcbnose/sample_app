2d93f59301dce56544dc738047d5fd8c63ec61b3
jcbnose <jcbnose@yahoo.com>
2010-11-03 15:00:22 -0700

    Romoved default Rails page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 SampleApp::Application.routes.draw do
-  get "pages/home"
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
 
-  get "pages/contact"
+  # You can have the root of your site routed with "root"
+  # just remember to delete public/index.html.
+  # root :to => "welcome#index"
 
-  get "pages/about"
-
-  get "pages/help"
+  root :to => 'pages#home'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
